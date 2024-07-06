3712619a3d880041edd66bda5b186be6fdacb971
amrdiab95 <50382915+amrdiab95@users.noreply.github.com>
2024-07-06 17:07:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// function getMessage(x){
+//     alert(x);
+//     alert(x);
+// }
+// getMessage("hi50");
+// getMessage("world");
+
+
+
+
+// function getMessage (x , number){  //paramter
+
+//     alert(x + number);
+// }
+
+
+// getMessage("hii",50); //argument
+// getMessage("world",60);
+// getMessage("Amr",80);
+
+
+
+// ================================================
+
+var counter=0;
+function dataincrement() {
+    counter++;
+    document.getElementById("demo").innerHTML=counter;
+}
+
+function datadecrement(){
+    counter--;
+    document.getElementById("demo").innerHTML=counter;
+}
